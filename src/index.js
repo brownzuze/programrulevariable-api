cfa0a6827389546034d0791f64db2646886871a0
brownzuze <bsc-05-16@cc.ac.mw>
2019-12-27 15:13:49 -0800

    commited by MISHECK ZUZE

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
+import Api from './api';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const apiVersion = 30;
+const developmentServer = 'https://play.dhis2.org/dev';
+const rootElement = document.getElementById('root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const withBaseUrl = baseUrl => {
+    Api.setConfig({
+        baseUrl: `${baseUrl}/api/${apiVersion}`,
+    });
+
+    ReactDOM.render(<App />, rootElement);
+};
+
+if (process.env.NODE_ENV === 'production') {
+    fetch('./manifest.webapp')
+        .then(response => response.json())
+        .then(manifest => {
+            withBaseUrl(`${manifest.activities.dhis.href}`);
+        })
+        .catch(e => {
+            console.error('Could not read manifest:', e);
+            ReactDOM.render(<code>No manifest found</code>, rootElement);
+        });
+} else {
+    withBaseUrl(developmentServer);
+}
